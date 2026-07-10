99cf4677da6814785611b3390342fa15032881bf
Karishma Rahaman <172750058+kar-ish-ma@users.noreply.github.com>
2026-07-10 22:10:04 +0530

    Added backend structure and API components

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,18 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+const registerUser =asyncHandler(async(req,res)=>{
+    //get user details from frontend
+    //validation-not empty 
+    //check if user already exists: username,email
+    //check if files exists avatar images
+    //upload them to cloudinary ,avatar
+    // create user object - create entry in db 
+    //remove password and refresh token foeld response
+    //check for user creation
+    //return res 
+    const{fullName,email,username, password}=req.body
+    console.log("email",email)
+
+})
+
+export {registerUser};
